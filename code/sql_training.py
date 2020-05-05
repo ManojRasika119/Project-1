a7009d1524c08b06c7570caa19f0b7c12ddcc642
Manoj_Rasika <01manojrasika10@gmail.com>
2020-05-06 00:20:32 +0300

    created SQLite Database table and updated with 5records

---
+++ code/sql_training.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table =  "CREATE TABLE users (id int, username text, password text)"
+cursor.execute(create_table)
+
+user = (1, 'Manoj', '1234')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+for user in user:
+    print (user)
+
+user = (2, 'Ashan', '4645')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+for user in user:
+    print (user)
+
+user = (3, 'Danangi', '9809')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+for user in user:
+    print (user)
+
+user = (4, 'Prathibha', '5731')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+for user in user:
+    print (user)
+
+user = (5, 'Jane', '86543')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+for user in user:
+    print (user)
+
+connection.commit()
+connection.close()
